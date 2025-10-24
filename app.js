7fa437ce31fbf9f9968c0b4a64fbb68d17c33d64
Mayank Bajaj <mayankbajaj672@gmail.com>
2025-10-24 08:18:28 +0530

    everything about env file

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import express from "express";
 
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("<h1>Hello Mayank!</h1>")
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}: http://localhost:3000/`)
+app.get("/about", (req, res) => {
+    res.send("<h1>About Page</h1>")
+})
+
+// console.log(process)
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}: http://localhost:${PORT}/`)
 })
